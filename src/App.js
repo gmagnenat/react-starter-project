db59e5064e95f77b4bfa1ee7bc1d050b6e687a50
dev <gmdev@MacBook-Pro-de-Gwenael.local>
2020-12-17 21:50:24 +0100

    create react starter project with parcel, prettier and eslint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Something will come here</h1>
+            <p>And maybe something else</p>
+        </div>
+    );
+};
+
+export default App;
